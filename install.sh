e8883e5b91d2cfcb706f7e52521f0f843466c396
Michael Whittaker <mjw297@cornell.edu>
2014-03-31 22:45:47 -0400

    Updated install script.

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 # install.sh - installation script
-#
-# This script will echo out the commands you need to set up this repository.
-# Simple redirect the output of this script into your .bashrc
-#
-#     ./install.sh >> ~/.bashrc
 
-echo "export TEXMFHOME=\"$(pwd):$TEXMFHOME\"" 
-echo "export PATH=\"$(pwd)/scripts:$PATH\""
-echo "export PYTHONPATH=\"$(pwd)/python:$PYTHONPATH\""
-echo "alias pythontex=\"python $(pwd)/pythontex/pythontex/pythontex.py\""
+BASHRC = ~/.bashrc
+
+echo "export TEXMFHOME=\"$(pwd):$TEXMFHOME\""          >> $BASHRC
+echo "export PATH=\"$(pwd)/scripts:$PATH\""            >> $BASHRC
+echo "export PYTHONPATH=\"$(pwd)/python:$PYTHONPATH\"" >> $BASHRC
